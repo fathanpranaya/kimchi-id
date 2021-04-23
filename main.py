cc529b2aa3acd47787f889defc8918846093b324
Fathan <fathan.pranaya@gmail.com>
2021-04-23 00:06:03 -0700

    added more info

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import uvicorn
 import requests
 import time
 import json
+import math
 
+from statistics import mean
 from datetime import datetime
 from fastapi import FastAPI
 
@@ -60,10 +62,16 @@ def get_coin_price(coin='xrp'):
     return coin_buy, coin_sell
 
 def get_coin_price_v2(indodax_price, coin='xrp'):
-    gopax_coin_res = requests.get('https://api.gopax.co.kr/trading-pairs/' + coin.upper() + '-KRW/ticker')
-    coin_sell = int(gopax_coin_res.json()['price'])
-    coin_buy = int(indodax_price['tickers'][coin.lower()+'_idr']['last'])
-    return coin_buy, coin_sell
+    gopax_coin_res = requests.get('https://api.gopax.co.kr/trading-pairs/' + coin.upper() + '-KRW/ticker').json()
+    coin_sell = int(gopax_coin_res['bid'])
+    coin_sell_vol = round(float(gopax_coin_res['quoteVolume'])/1e9, 2)
+
+    
+    buy = indodax_price['tickers'][coin.lower()+'_idr']
+    coin_buy = int(buy['buy'])
+    coin_buy_vol = round(float(buy['vol_idr'])/1e9, 2)
+
+    return coin_buy, coin_sell, coin_buy_vol, coin_sell_vol
 
 @app.get("/test")
 async def test(request: Request):
@@ -103,47 +111,59 @@ def calc_kimchi(idr, coin_buy, coin_sell, coin_name):
     
     return krw_bank
 
-
 def calculate_kimchi(idr: int = 100):
     # EXTERNAL API CALL
     # HANPASS_RATE = get_hanpass_rate()
     # SENTBE_RATE = get_sentbe_rate()
     BANDAR_RATE = 12.75
     indodax_price = requests.get('https://indodax.com/api/ticker_all').json()
+    ex_rate = mean([SENTBE_RATE, HANPASS_RATE, BANDAR_RATE])
+    
+    fear_pairs = requests.get('https://datavalue.dunamu.com/api/fearindex').json()['pairs']
+    fear_levels = {}
+    for pair in fear_pairs:
+        fear_levels[pair['currency']] = "{:.2f}".format(float(pair['score']))
 
     IDR = idr * 1e6
     coins = []
     for coin_name in coin_names:
-        coin_buy, coin_sell = get_coin_price_v2(indodax_price, coin_name)
+        coin_buy, coin_sell, coin_buy_vol, coin_sell_vol = get_coin_price_v2(indodax_price, coin_name)
         coin_kimchi = calc_kimchi(IDR, coin_buy, coin_sell, coin_name)
-        rows = [
-            {
-                'remittance': 'Sentbe',
-                'rate': "{:.2f}".format(SENTBE_RATE),
-                'kimchi': "{:.2f}".format(((coin_kimchi * SENTBE_RATE / IDR) - 1) * 100)
-            },
-            {
-                'remittance': 'Hanpass',
-                'rate': "{:.2f}".format(HANPASS_RATE),
-                'kimchi': "{:.2f}".format(((coin_kimchi * HANPASS_RATE / IDR) - 1) * 100)
-            },
-            {
-                'remittance': 'Bandar',
-                'rate': "{:.2f}".format(BANDAR_RATE),
-                'kimchi': "{:.2f}".format(((coin_kimchi * BANDAR_RATE / IDR) - 1) * 100)
-            },
-        ]
+        coin_fear = fear_levels.get(coin_name.upper())
+        
+        # rows = [
+        #     {
+        #         'remittance': 'Sentbe',
+        #         'rate': "{:.2f}".format(SENTBE_RATE),
+        #         'kimchi': "{:.2f}".format(((coin_kimchi * SENTBE_RATE / IDR) - 1) * 100)
+        #     },
+        #     {
+        #         'remittance': 'Hanpass',
+        #         'rate': "{:.2f}".format(HANPASS_RATE),
+        #         'kimchi': "{:.2f}".format(((coin_kimchi * HANPASS_RATE / IDR) - 1) * 100)
+        #     },
+        #     {
+        #         'remittance': 'Bandar',
+        #         'rate': "{:.2f}".format(BANDAR_RATE),
+        #         'kimchi': "{:.2f}".format(((coin_kimchi * BANDAR_RATE / IDR) - 1) * 100)
+        #     },
+        # ]
         coin_ctx = {
             'coin_name': coin_name,
             'gopax_price': coin_sell,
             'indodax_price': coin_buy,
-            'rows': rows,
+            'coin_fear': coin_fear,
+            'coin_buy_vol': coin_buy_vol,
+            'coin_sell_vol': coin_sell_vol,
+            'kimchi': "{:.2f}".format(((coin_kimchi * ex_rate / IDR) - 1) * 100)
+            # 'rows': rows,
         }
         coins.append(coin_ctx)    
 
     context = {
         'ts': datetime.now().strftime("%y/%m/%d %H:%M"),
         'coins': coins,
+        'kurs': ex_rate,
     }
 
     return context
